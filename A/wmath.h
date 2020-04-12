c0909dee1f9000ee3bf2da2489775e50760150d8
Ethan Ferguson <sometimescode@gmail.com>
2020-04-12 19:07:29 -0400

    new branch

--- A/wmath.h
+++
@@ -1,9 +0,0 @@
-
-#pragma once
-
-#include <stdio.h>
-#include <algorithm>
-template<class T>
-inline double wclamp(const T& a, const T& lo, const T& hi){
-    return std::min(std::max(a, lo), hi);
-}
